78a6d153027cc095c2014dce23ac3f310372acb5
AngelinaRS <candy26y_@hotmail.com>
2015-07-23 11:32:00 -0600

    Main menu

--- application.py
+++ application.py
@@ -1,3 +1,31 @@
-# Aqui escribe tu codigo
+"""Register-machine"""
 
-print "Hello World"
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+def reset():
+    """This cleans the screen"""
+    os.system("reset")
+
+def menu():
+    """This saves the menu"""
+
+    print "\nWelcome to Register Machine\n"
+    print "1. Add an Item\n"
+    print "2. Sell Articles\n"
+    print "3. Exit\n"
+
+    answer_menu = True
+    while answer_menu == True:
+        choose_user = raw_input(" - ") #This saves the election of the user
+        if choose_user == "3":
+            reset()
+            sys.exit() #This forces the output of interpreter
+        else:
+            reset()
+            print "* Election invalid, choose one option valid"
+            menu()
+menu()
