c0a20ce7115efd091deaf622d200f8b55f68ce62
AngelinaRS <candy26y_@hotmail.com>
2015-08-12 09:43:05 -0600

    Sell products

--- application.py
+++ application.py
@@ -6,52 +6,108 @@
 import os
 import sys
 
-ADD_PRODUCTS = {}
+ADD_PRODUCTS = {} #This saves the products added by manager
+SAVE_EXISTENT = [] #This saves the products existent
 
 def reset():
     """This cleans the screen"""
     os.system("reset")
 
+#Test #1
 def name_product():
+    """This saves the name of the product"""
     reset()
-    while True:
-        product = raw_input("Add the product: ")
-        if product.isalpha() == True:
-            return product.lower()
-        else:
-            print "Insert a valid name"
+    if __name__ == '__main__':
+        while True:
+            product = raw_input("Add the product: ")
+            if product.isalpha(): #This verifies if the product that has inserted, has a valid name
+                return product.lower()
+            else:
+                print "Insert a valid name"
+    return product
 
+#Test 2
 def price_product():
-    while True:
-        price = raw_input("Add the price: ")
-        try:
-            price = float(price)
-            return price
-        except:
-            print "Insert a number"
-
+    """This saves the price of the product"""
+    if __name__ == '__main__':
+        while True:
+            price = raw_input("Add the price: ")
+            try:
+                price = float(price) #This verifies if the price is a number
+                return price
+            except ValueError:
+                print "Insert a number"
+    return price
 
 def add_item(product, price):
+    """This saves the products and prices in a dictionary"""
     ADD_PRODUCTS[product] = price
 
-def add_article():
-    name_article = name_product()
-    product_price = price_product()
+
+def add_product_with_price():
+    """This adds the product with their respective price"""
+
+    product = name_product()
+    price = price_product()
+    add_item(product, price)
 
     while True:
         another = raw_input("Do you want to insert another article? y/n ")
         another = another.lower()
         if another == "y":
-            name_product()
-            price_product()
+            product = name_product()
+            price = price_product()
+            add_item(product, price)
         elif another == "n":
             reset()
             menu()
         else:
             reset()
             print "Insert y or n"
+#Test 3
+def insert_product_to_sell():
+    """This inserts the product to buy"""
+    sell = raw_input(" - ")
+    sell = sell.lower()
+    return sell
 
+def sell_products():
+    """This verificate if the product is available"""
 
+    print "Insert the products you want to buy: "
+    print "Press done to finish"
+    while True:
+        sell = insert_product_to_sell()
+        if ADD_PRODUCTS.has_key(sell) == True: #Verifies if the product belongs to the dictionary
+            print "%.2f" % ADD_PRODUCTS.get(sell) #This prints its price respective
+            SAVE_EXISTENT.append(sell) #This adds the product to a list
+        elif sell == "done":
+            reset()
+            menu()
+        else:
+            print "This product is not available"
+
+def products_to_sell():
+    """This shows the products in sale"""
+
+    print "These are the products in sale"
+    for key, value in ADD_PRODUCTS.iteritems():
+        print "%s: Q%.2f" % (key, value)
+
+def ask_if_want():
+    """This asks to the user if wants to see the products"""
+    while True:
+        ask = raw_input("Do you want to see the products in sale? ")
+        ask = ask.lower()
+        if ask == "y":
+            reset()
+            products_to_sell()
+            sell_products()
+        elif ask == "n":
+            reset()
+            sell_products()
+        else:
+            print "Election invalid"
 def menu():
     """This saves the menu"""
 
@@ -65,13 +121,19 @@ def menu():
         choose_user = raw_input(" - ") #This saves the election of the user
         if choose_user == "1":
             reset()
-            add_article()
+            add_product_with_price()
+        elif choose_user == "2":
+            reset()
+            ask_if_want()
         elif choose_user == "3":
             reset()
             sys.exit() #This forces the output of interpreter
         else:
             reset()
             print "*Election invalid, choose a valid option"
+            print SAVE_EXISTENT
             menu()
 menu()
 
+if __name__ == '__main__':
+    menu()
