968a37ec3478fbdf90061566a69cb09876b11895
AngelinaRS <candy26y_@hotmail.com>
2015-09-22 11:14:41 -0600

    Verified

--- unit_test.py
+++ unit_test.py
@@ -4,41 +4,29 @@ import unittest
 class RegisterMachineTest(unittest.TestCase):
     #test function 1
     def test_product_isalpha(self):
-        self.assertTrue(application.product_isalpha("hay"))
+        self.assertTrue(application.product_isalpha("Hay"))
         self.assertFalse(application.product_isalpha("5"))
 
     #test function 2
-    def test_name_product(self):
-        self.assertTrue(application.name_product("PRO").isalpha())
-        self.assertTrue(application.name_product(5))
+    def test_minuscule(self):
+        self.assertTrue(application.minuscule("Dulce").islower())
 
     #test function 3
-    def test_price_product(self):
-        self.assertTrue(application.price_product(5))
-
-    #test function 4
-    def test_insert_product_to_sell(self):
-        self.assertTrue(application.insert_product_to_sell("HAY").islower())
-
-    #test function 5
     def test_gold_card(self):
         self.assertEqual(application.gold_card(100), 5)
 
-    #test function 6
+    #test function 4
     def test_silver_card(self):
         self.assertEqual(application.silver_card(100),2)
 
-    #test finction 7
-    def test_subtract_discount(self):
-        self.assertEqual(application.subtract_discount(100,50), 50)
-
-    #test function 8
+    #test function 5
     def test_tax(self):
-        self.assertEqual(application.tax(100), 12)
+        self.assertEqual(application.tax(100, 0), 12)
 
-    #test function 9
+    #test function 7
     def test_total_final(self):
-        self.assertEqual(application.total_final(100, 12), 112)
+        self.assertEqual(application.total_final(100, 0, 12), 112)
+
 
 if __name__ == "__main__":
     unittest.main()
